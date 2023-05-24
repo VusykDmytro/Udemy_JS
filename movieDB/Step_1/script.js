398ae991b5f8660a7e9d908406db46402d7850ff
Dmytro <d.vusyk@gmail.com>
2023-05-24 22:32:38 +0300

    add MovieDB step_1

---
+++ movieDB/Step_1/script.js
@@ -0,0 +1,24 @@
+
+
+// 1
+
+const numberOfFilms = prompt('Яку кількість фільмів Ви вже перегянули?');
+const lastMovie = prompt('Один із останніх переглянутих фільмів?');
+const rating = prompt('На скільки оціните його?');
+
+// 2
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+// 3
+
+
+
+personalMovieDB.movies.lastMovie = lastMovie;
+personalMovieDB.movies.lastMovie.rating;
